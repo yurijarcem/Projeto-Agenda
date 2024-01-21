e247cbe5f385fd7de76cef584abbef11568c5c49
Yuri Jarcem <yurijarcem9110@gmail.com>
2024-01-21 11:07:14 -0300

    Initial commit

---
+++ config/connection.php
@@ -0,0 +1,13 @@
+<?php
+    $host = "localhost";
+    $dbname = "Agenda";
+    $user = "root";
+    $pass = "";
+
+    try{
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e){
+        $error = $e->getMessage();
+        echo "Erro: $error";
+    }
